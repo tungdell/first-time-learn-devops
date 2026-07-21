b59db6ede31ba8632af66fc371e73c86c58077db
Tùng <tranductungk8e@gmail.com>
2026-07-21 14:45:16 +0700

    fix yaml

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello tung!");
+console.log("i try learn CI/CD with github action");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("starting test");
+setTimeout(() => console.log("waiting 3 seconds"), 3000);
+console.log("test finished");
